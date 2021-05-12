d9d78b1c5bae323d655833fa06c920feb13b562c
Ste Griffiths <stephen.griffiths@nsg.com>
2021-05-12 17:02:16 +0100

    Scripting

--- build.js
+++ build.js
@@ -1,3 +1,14 @@
 console.log("I AM THE BUILD SCRIPT");
 let y = 24 * 7;
 console.log("I CAN DO MATHS. LOOK: ", y, "... SEE!");
+
+const apiKey = process.env.kino_key;
+const spaceId = "yk-V28eDrL3_WRznxA45a";
+
+console.log("API key is: ", apiKey);
+console.log("Space ID is: ", spaceId);
+
+const apiRoot = "https://api.kinopio.club";
+const requestUri = apiRoot + "/space/" + spaceId;
+
+console.log("Request", requestUri);
